528537897697f0f0e20848bf2cfb8d451ab25286
bassam edelbi <bassamedelbi360@gmail.com>
2021-01-24 12:37:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function testFunc() {
 }
 
 
-superbowlWin = (record) => {
-  let result = record.find( record => record.result === "W" )
-  return !!result ? result.year : undefined
+superbowlWin=(record) => {
+  let result = record.find(record => record.result==="W")
+  return !!result ? result.year: undefined
 }
